1c12675d52d3a3957fafefb8bd2d31279ff7dd28
WardMore <rafaelmserranov@gmail.com>
2024-03-14 03:12:14 -0300

    se corrigen rutas relativas

--- js/menu.js
+++ js/menu.js
@@ -2,21 +2,21 @@
 $("#depositarDineroModal").on("shown.bs.modal", function (e){
     //Redirige a la página de depositar dinero después de 2 segundos
     setTimeout(function(){
-        window.location = "../html/deposit.html"; }, 2000);
+        window.location = "/html/deposit.html"; }, 2000);
     
 });
 
 $("#transferirDineroModal").on("shown.bs.modal", function (e){
     //Redirige a la página de transferir dinero después de 2 segundos
     setTimeout(function(){
-        window.location = "../html/sendmoney.html"; }, 2000);
+        window.location = "/html/sendmoney.html"; }, 2000);
     
 });
 
 $("#movimientosModal").on("shown.bs.modal", function (e){
     //Redirige a la página de movimientos después de 2 segundos
     setTimeout(function(){
-        window.location = "../html/transactions.html"; }, 2000);
+        window.location = "/html/transactions.html"; }, 2000);
     
 });
 
